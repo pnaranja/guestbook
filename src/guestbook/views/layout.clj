34cfdfeceefc40ce0316c1d3cdf0ea3b6e3f9305
pnaranja <paul.naranja@gmail.com>
2015-06-20 02:13:50 -0700

    Added bootstrap css link

--- src/guestbook/views/layout.clj
+++ src/guestbook/views/layout.clj
@@ -4,7 +4,9 @@
 (defn common [& body]
   "Called by the home route which supplies the body of the html"
   (html5
+    [:link {:rel "stylesheet" :href "//maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"}]
     [:head
      [:title "Welcome to guestbook"]
      (include-css "/css/screen.css")]
     [:body body]))
+
